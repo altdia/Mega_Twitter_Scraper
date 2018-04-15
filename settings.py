f6735d4be972824e423432b0581e161e0ae1ef2f
altdia <38409986+altdia@users.noreply.github.com>
2018-04-15 19:11:13 -0400

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,6 +1,5 @@
 TRACK_TERMS = ["#GodBless", "#CrisisActor", "#CrisisActors", "#ForGodAndCountry", "#AmericanHeroes", "#ReleaseTheCures", "#MAGA", "#GreatAwakening", "#WeThePeople", "#FakeNews", "#Treason", "#ReleaseEverything", "#QAnon", "#AmericaFirst", "#ObamaGate","#ReleaseTheFootage", "#Qanon8chan", "#tillerson","Trump", "Tillerson", "North Korea", "Russia", "Clinton", "Illegal Immigrants", "#Q", "election"]
 CONNECTION_STRING = "postgresql://username:password@host:port/database"
-CSV_NAME = "bots.csv"
 TABLE_NAME = "botpull"
 
 try:
